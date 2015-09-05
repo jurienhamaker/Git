c999a7cca64bea442c85ee05c17342f63856da61
Juriën Hamaker <jurienhamaker@Hotmail.com>
2015-09-05 23:20:36 +0200

    init class

---
+++ src/Git.php
@@ -0,0 +1,7 @@
+<?php 
+namespace HazeDevelopment;
+
+Class Git
+{
+    
+}
---
+++ src/GitServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HazeDevelopment;
+
+use Illuminate\Support\ServiceProvider;
+
+class GitServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
